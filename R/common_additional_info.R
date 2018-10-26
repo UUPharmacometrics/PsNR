7f3979ed4fba4442043340ab1bccc6c68fcabc3d
Rikard Nordgren <rikard.nordgren@farmbio.uu.se>
2018-10-26 18:05:49 +0200

    Fix additional_info for new version of devtools

--- R/common_additional_info.R
+++ R/common_additional_info.R
@@ -4,7 +4,15 @@
 #' @return A list of three character elements (PsN_version, NONMEM_version and R_version) and one dataframe element (R_packages).
 additional_info <- function(yaml_file) {
   yaml_list <- yaml::yaml.load_file(yaml_file)
-  R_packages <- as.data.frame(devtools::session_info()[[2]])[,c("package","version","source")]
+
+  R_packages <- as.data.frame(devtools::session_info()[[2]])
+  if(any(colnames(R_packages)=="version")) {
+      R_packages <- R_packages[,c("package","version","source")]
+  } else {
+      R_packages <- R_packages[,c("package","loadedversion","source")] %>%
+          dplyr::rename(version=loadedversion)
+  }
+
   out <- list(PsN_version=yaml_list$PsN_version,
               NONMEM_version=yaml_list$NONMEM_version,
               R_version=yaml_list$R_version,
