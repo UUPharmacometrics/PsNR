6b38de5cb58ba578b3c24c32da21af79cdb1020b
Svetlana Freiberga <svetlana.freiberga@farmbio.uu.se>
2018-12-06 13:16:46 +0100

    R: replace plyr with dplyr functions

--- R/bootstrap_format.dofv.data.R
+++ R/bootstrap_format.dofv.data.R
@@ -18,12 +18,17 @@ format_dofv_data <- function(dofv.raw.results.file,raw.results.file,est.param.na
   boot$minimization_successful <- "yes+no"
   
   # Create reference chisquare (for dOFV)
-  ref <- data.frame("deltaofv"=qchisq(seq(0,0.99,0.01),df=length(est.param.names)),"rownames"=seq(0,0.99,0.01),"METHOD"="REF")
+  ref <- data.frame("deltaofv"=qchisq(seq(0,0.99,0.01),df=length(est.param.names)),"rownames"=seq(0,0.99,0.01),"METHOD"="REF",stringsAsFactors = F)
   
   # Merge and format datasets
-  all <- plyr::rbind.fill(boot,boot_term,ref)
+  all <- dplyr::bind_rows(boot,boot_term,ref)
   all$minimization_successful[is.na(all$minimization_successful)] <- "yes"
-  df_est   <- plyr::ddply(all,.(METHOD,minimization_successful), summarise, "df"=round(mean(deltaofv,na.rm=T),1))  # get df for each distribution
+  # get df for each distribution
+  df_est <- all %>%
+    dplyr::group_by(METHOD,minimization_successful) %>%
+    dplyr::summarise(df=round(mean(deltaofv,na.rm=T),1)) %>%
+    dplyr::ungroup()
+  
   df_est[df_est$METHOD=="REF",]$df <- length(est.param.names) # replace with true df to avoid random noise
   
   out <- list(dofv.raw.results.data=dofv.raw.results.data,
