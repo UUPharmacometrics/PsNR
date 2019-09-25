196eb28cedea4e5eaa6f8bff34c8955a7d5ef8b4
Sebastian Ueckert <sebastian.ueckert@gmail.com>
2019-09-25 10:33:36 +0200

    add function to evaluate expression safely

--- R/common_conditions.R
+++ R/common_conditions.R
@@ -1,3 +1,20 @@
+#' Run expression and retain errors
+#' 
+#' The function allows to evaluates an expression and store eventual errors in the results data structure. 
+#' 
+#' @param expr Expression to evaluate
+#'
+#' @return A list with entries results and error (one of them is NA)
+#' @export
+#'
+#' @examples
+#' do_safely("a"/1)
+do_safely <- function(expr){
+  res <- try(rlang::with_abort(expr), silent = TRUE)
+  if(inherits(res, "try-error")) return(list(result = NA, error = res))
+  else return(list(result = res, error = NA))
+}
+
 cnd_file_not_found <- function(path)
   rlang::error_cnd("file_not_found", path = path,
                    message = paste0("File '",path,"' not found."))
