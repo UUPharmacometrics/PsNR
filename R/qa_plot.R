97f34b3e21f4195397fd7128eec6f8d761bb0bc9
Sebastian Ueckert <sebastian.ueckert@gmail.com>
2019-10-16 16:53:37 +0200

    QA: addition of DVID in VA plot

--- R/qa_plot.R
+++ R/qa_plot.R
@@ -20,12 +20,13 @@ plot_result <- function(r){
 prepare_va_cov_plot <- function(qa_results){
   frem_path <- qa_results$files$frem$path
   derivatives_lst <- qa_results$files$linearize$derivatives_lst
-  
+  dvid <- NULL
+  if(qa_results$options$dvid_name!='') dvid <- qa_results$options$dvid_name 
   p <- tryCatch({
     input_base <- vaplot::prepare_va_nm(derivatives_lst)
-    res_base <- vaplot::compute_va(input_base)
+    res_base <- vaplot::compute_va(input_base, facets = dvid)
     input_frem <- vaplot::prepare_va_frem(frem_path)
-    res_frem <- vaplot::compute_va(input_frem)
+    res_frem <- vaplot::compute_va(input_frem, facets = dvid)
     vaplot::plot_va_compare(`without covariates` = res_base, 
                                  `with covariates` = res_frem, 
                                  smooth = TRUE)+
