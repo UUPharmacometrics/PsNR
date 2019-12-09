8213dade7fa4a69d668d1e928bcb7b9df201e43f
Rikard Nordgren <rikard.nordgren@farmbio.uu.se>
2019-12-09 13:30:03 +0100

    Fix bug in results_summary.yaml generation
    Bug caused crash when having DVID and skipping resmod

--- R/qa_results_summary.R
+++ R/qa_results_summary.R
@@ -18,7 +18,7 @@
 #' @param skip A character vector with names of the skipped parts in the qa run.
 #' @param idv_all A character vector with all idv names.
 #'
-#' @return A yaml file results_summary.yaml for the specific qa run. 
+#' @return A yaml file results_summary.yaml for the specific qa run.
 #' @export
 yaml_summary <- function(model.filename,
                          ofv_table,
@@ -56,7 +56,7 @@ yaml_summary <- function(model.filename,
   iov_list <- check_argument_existance(iov_list)
   all_outl_list <- check_argument_existance(all_outl_list)
   ofv_table <- check_argument_existance(ofv_table)
-  
+
   #add nonlinear and linearized ofv values to the tree
   tree <- list(
     model_filename = model.filename,
@@ -65,7 +65,7 @@ yaml_summary <- function(model.filename,
     linearized_base_model_ofv_after_estimation = ofv_table[3,2],
     sum_of_individual_ofv_values = ofv_table[4,2]
   )
-  
+
   # if nonlinear=TRUE then ofv_table will not exist
   if(!is.null(ofv_table)) {
     tree <- list(
@@ -86,7 +86,7 @@ yaml_summary <- function(model.filename,
       sum_of_individual_ofv_values = NA
     )
   }
-  
+
  #check if dvid exists
   if(dvid_name!='') {
     tree$dvid_exists <- TRUE
@@ -96,46 +96,48 @@ yaml_summary <- function(model.filename,
   }
   tree$skip <- skip
   tree$all_idv <- idv_all
-  
+
   #overview table
   ovw_table <- overview_table_list$overview_table
   colnames(ovw_table)[1] <- "name"
   group_name <- rep(overview_table_list$rgroup_names,times = overview_table_list$n.rgroup)
   ovw_table <- ovw_table %>%
     dplyr::mutate(group_name=rep(overview_table_list$rgroup_names,times = overview_table_list$n.rgroup))
-  
+
   # overview (structural)
   tree$overview <- list()
   overview <- ovw_table %>%
-    dplyr::filter(group_name=="Structural Model") 
-  if(tree$dvid_exists==TRUE) {
-    overview <- overview %>%
-      dplyr::mutate(dvid=dplyr::case_when(grepl(dvid_name,name) ~ name,
+    dplyr::filter(group_name=="Structural Model")
+  if (overview$dOFV != 'SKIPPED') {
+    if(tree$dvid_exists==TRUE) {
+        overview <- overview %>%
+        dplyr::mutate(dvid=dplyr::case_when(grepl(dvid_name,name) ~ name,
                             TRUE ~ as.character(NA)),
              dvid=as.numeric(gsub("\\D", "", dvid))) %>%
-      tidyr::fill(dvid, .direction = "down") %>%
-      dplyr::filter(!(grepl(dvid_name,name)))
-    
-    tree$overview$structural <- list()
-    for(i in 1:length(unique(overview$dvid))) {
-      tree$overview$structural[[i]] <- list()
-      tree$overview$structural[[i]]$dvid_value <- unique(overview$dvid)[i]
-      overview_dvid <- overview %>%
-        dplyr::filter(dvid==unique(overview$dvid)[i])
+        tidyr::fill(dvid, .direction = "down") %>%
+        dplyr::filter(!(grepl(dvid_name,name)))
 
-      for(j in 1:nrow(overview_dvid)) {
-        tree$overview$structural[[i]]$dofv[[overview$name[j]]] <- overview_dvid$dOFV[j]
-        tree$overview$structural[[i]]$additional_param[[overview$name[j]]] <- overview_dvid$`Additional parameters`[j]
-      }
-    }
-  } else {
-    tree$overview$structural[[1]] <- list()
-    for(i in 1:nrow(overview)) {
-      tree$overview$structural[[1]]$dofv[[overview$name[i]]] <- overview$dOFV[i]
-      tree$overview$structural[[1]]$additional_param[[overview$name[i]]] <- overview$`Additional parameters`[i]
+        tree$overview$structural <- list()
+        for(i in 1:length(unique(overview$dvid))) {
+            tree$overview$structural[[i]] <- list()
+            tree$overview$structural[[i]]$dvid_value <- unique(overview$dvid)[i]
+            overview_dvid <- overview %>%
+            dplyr::filter(dvid==unique(overview$dvid)[i])
+
+            for(j in 1:nrow(overview_dvid)) {
+                tree$overview$structural[[i]]$dofv[[overview$name[j]]] <- overview_dvid$dOFV[j]
+                tree$overview$structural[[i]]$additional_param[[overview$name[j]]] <- overview_dvid$`Additional parameters`[j]
+            }
+        }
+    } else {
+        tree$overview$structural[[1]] <- list()
+        for(i in 1:nrow(overview)) {
+            tree$overview$structural[[1]]$dofv[[overview$name[i]]] <- overview$dOFV[i]
+            tree$overview$structural[[1]]$additional_param[[overview$name[i]]] <- overview$`Additional parameters`[i]
+        }
     }
   }
-  
+
   # overview (parameter variability)
   tree$overview$param_variability <- list()
   par_variab <- ovw_table %>%
@@ -144,7 +146,7 @@ yaml_summary <- function(model.filename,
     tree$overview$param_variability$dofv[[par_variab$name[i]]] <- par_variab$dOFV[i]
     tree$overview$param_variability$additional_param[[par_variab$name[i]]] <- par_variab$`Additional parameters`[i]
   }
-  
+
   # overview (frem)
   tree$overview$frem <- list()
   frem_part <- ovw_table %>%
@@ -152,7 +154,7 @@ yaml_summary <- function(model.filename,
     dplyr::slice(1)
   tree$overview$frem$dofv[[frem_part$name]] <- frem_part$dOFV
   tree$overview$frem$additional_param[[frem_part$name]] <- frem_part$`Additional parameters`
-  
+
   # overview (scm)
   tree$overview$scm <- list()
   scm_part <- ovw_table %>%
@@ -160,38 +162,40 @@ yaml_summary <- function(model.filename,
     dplyr::slice(2)
   tree$overview$scm$dofv[[scm_part$name]] <- scm_part$dOFV
   tree$overview$scm$additional_param[[scm_part$name]] <- scm_part$`Additional parameters`
-  
+
   # overview (ruv)
   overview <- ovw_table %>%
-    dplyr::filter(group_name=="Residual Error Model") 
-  if(tree$dvid_exists==TRUE) {
-    overview <- overview %>%
-      dplyr::mutate(dvid=dplyr::case_when(grepl(dvid_name,name) ~ name,
+    dplyr::filter(group_name=="Residual Error Model")
+  if (overview$dOFV != 'SKIPPED') {
+    if(tree$dvid_exists==TRUE) {
+        overview <- overview %>%
+        dplyr::mutate(dvid=dplyr::case_when(grepl(dvid_name,name) ~ name,
                             TRUE ~ as.character(NA)),
              dvid=as.numeric(gsub("\\D", "", dvid))) %>%
-      tidyr::fill(dvid, .direction = "down") %>%
-      dplyr::filter(!(grepl(dvid_name,name)))
-    
-    tree$overview$residual_error <- list()
-    for(i in 1:length(unique(overview$dvid))) {
-      tree$overview$residual_error[[i]] <- list()
-      tree$overview$residual_error[[i]]$dvid_value <- unique(overview$dvid)[i]
-      overview_dvid <- overview %>%
-        dplyr::filter(dvid==unique(overview$dvid)[i])
-      
-      for(j in 1:nrow(overview_dvid)) {
-        tree$overview$residual_error[[i]]$dofv[[overview$name[j]]] <- overview_dvid$dOFV[j]
-        tree$overview$residual_error[[i]]$additional_param[[overview$name[j]]] <- overview_dvid$`Additional parameters`[j]
-      }
-    }
-  } else {
-    tree$overview$residual_error[[1]] <- list()
-    for(i in 1:nrow(overview)) {
-      tree$overview$residual_error[[1]]$dofv[[overview$name[i]]] <- overview$dOFV[i]
-      tree$overview$residual_error[[1]]$additional_param[[overview$name[i]]] <- overview$`Additional parameters`[i]
+        tidyr::fill(dvid, .direction = "down") %>%
+        dplyr::filter(!(grepl(dvid_name,name)))
+
+        tree$overview$residual_error <- list()
+        for(i in 1:length(unique(overview$dvid))) {
+            tree$overview$residual_error[[i]] <- list()
+            tree$overview$residual_error[[i]]$dvid_value <- unique(overview$dvid)[i]
+            overview_dvid <- overview %>%
+            dplyr::filter(dvid==unique(overview$dvid)[i])
+
+            for(j in 1:nrow(overview_dvid)) {
+                tree$overview$residual_error[[i]]$dofv[[overview$name[j]]] <- overview_dvid$dOFV[j]
+                tree$overview$residual_error[[i]]$additional_param[[overview$name[j]]] <- overview_dvid$`Additional parameters`[j]
+            }
+        }
+    } else {
+        tree$overview$residual_error[[1]] <- list()
+        for(i in 1:nrow(overview)) {
+            tree$overview$residual_error[[1]]$dofv[[overview$name[i]]] <- overview$dOFV[i]
+            tree$overview$residual_error[[1]]$additional_param[[overview$name[i]]] <- overview$`Additional parameters`[i]
+        }
     }
   }
-  
+
   # overview (cdd)
   tree$overview$infl_id <- list()
   cdd_part <- ovw_table %>%
@@ -199,7 +203,7 @@ yaml_summary <- function(model.filename,
     dplyr::slice(1)
   tree$overview$infl_id$dofv[[cdd_part$name]] <- cdd_part$dOFV
   tree$overview$infl_id$additional_param[[cdd_part$name]] <- cdd_part$`Additional parameters`
-  
+
   # overview (simeval)
   tree$overview$outliers <- list()
   simeval_part <- ovw_table %>%
@@ -207,7 +211,7 @@ yaml_summary <- function(model.filename,
     dplyr::slice(1)
   tree$overview$outliers$dofv[[simeval_part$name]] <- simeval_part$dOFV
   tree$overview$outliers$additional_param[[simeval_part$name]] <- simeval_part$`Additional parameters`
-  
+
   #structural part
   if(!is.null(resmod_structural_details)) {
     for(i in 1:length(resmod_structural_details)) {
@@ -217,8 +221,8 @@ yaml_summary <- function(model.filename,
       resmod_structural_details_modified$first_table <- NULL
       tree$structural[[i]] <- resmod_structural_details_modified
     }
-  } 
-  
+  }
+
   #parameter variability tables
   if(all(skip!="transform")) {
     tree$parameter_variability <- list()
@@ -238,17 +242,17 @@ yaml_summary <- function(model.filename,
       tree$parameter_variability$add_etas_table <- iov_list
     }
   }
-  
+
   #frem table
   if(all(skip!="frem")) {
     tree$frem <- frem_table_list
   }
-  
+
   #scm tables
   if(all(skip!="scm")) {
     tree$scm <- scm_table_list
   }
-  
+
   #resmod tables
   if(all(skip!="resmod")) {
     tree$resmod <- list()
@@ -271,13 +275,13 @@ yaml_summary <- function(model.filename,
     tree$cdd$ii_table <- ii_list$ii_table
     tree$cdd$infl_id <- ii_list$infl_id
   }
-  
+
   #outliers tables
   if(all(skip!="simeval")) {
     outlier_table_list_modified <- outlier_table_list
     outlier_table_list_modified$fig_height_outl <- NULL
     tree$simeval <- outlier_table_list_modified
-    
+
     #overall outliers table
     if(!is.null(all_outl_list)) {
       all_outl_list_modified <- all_outl_list
