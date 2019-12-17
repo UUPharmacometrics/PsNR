8fcd2551e2f5be923f14d0452809c6fff2d90c90
Rikard Nordgren <rikard.nordgren@farmbio.uu.se>
2019-12-17 13:31:59 +0100

    Remove need for extra_table_columns array from PsN

--- R/qa_calc_and_add_shift_from_cwres.R
+++ R/qa_calc_and_add_shift_from_cwres.R
@@ -1,11 +1,11 @@
 #' @export
-.calc_and_add_shift_from_cwres <- function(structural_details_table, orig_ext_file, base_dataset, extra_table, extra_table_colnames, idv, dvid, dvid_name){
+.calc_and_add_shift_from_cwres <- function(structural_details_table, orig_ext_file, base_dataset, extra_table, idv, dvid, dvid_name){
   # .ext file with the final estimates
   final_estimates <- read.table(orig_ext_file, skip=1, header=T) %>%
     dplyr::filter(ITERATION==-1000000000)
-  
+
   # populate omega from .ext file
-  omega_values <- final_estimates %>% dplyr::select(dplyr::starts_with("OMEGA")) 
+  omega_values <- final_estimates %>% dplyr::select(dplyr::starts_with("OMEGA"))
   # create omega matrix
   numeration <- sub('.*OMEGA\\.','',colnames(omega_values)) %>% substr(., 1, nchar(.)-1)
   dim_omega <- length(unique(as.numeric(sub('\\..*','',numeration))))
@@ -20,7 +20,7 @@
       omega_matrix[second,first] <- omega_values[1,i]
     }
   }
-  
+
   # populate sigma from ext file
   sigma_values <- final_estimates %>% dplyr::select(dplyr::starts_with("SIGMA"))
   numeration <- sub('.*SIGMA\\.','',colnames(sigma_values)) %>% substr(., 1, nchar(.)-1)
@@ -36,12 +36,11 @@
       sigma_matrix[second,first] <- sigma_values[1,i]
     }
   }
-  
+
   # get table with IDV values
   idv_df <- read.table(extra_table, skip = 1, header=T) #%>%
     #dplyr::filter(CWRES!=0)
-  colnames(idv_df) <- extra_table_colnames
-  
+
   cpred_column <- idv_df$CPRED
   if(any(colnames(idv_df)== dvid_name)) {
     dvid_column_nr <- which(colnames(idv_df)== dvid_name)
@@ -53,7 +52,7 @@
   idv_df <- idv_df %>%
     dplyr::select_(idv) %>%
     dplyr::rename_(idv=idv)
-  
+
   # get derivatives from linearized model
   #mean_shifts_table <- read.table(base_dataset, header=T) %>%
   mean_shifts_table <- read.table(base_dataset, header=T, skip=1) %>%
@@ -83,16 +82,16 @@
                  bin_index = .$bin_index,
                  ipred = .$CPRED)
     }) %>%
-    
+
     dplyr::group_by(bin_index) %>%
-    dplyr::summarise(relative_shift = ifelse((any(mean_shifts_table$CPRED > 0) && any(mean_shifts_table$CPRED < 0)),NA,100*mean(shift/ipred)), 
+    dplyr::summarise(relative_shift = ifelse((any(mean_shifts_table$CPRED > 0) && any(mean_shifts_table$CPRED < 0)),NA,100*mean(shift/ipred)),
               shift = mean(shift),nobs=n()) %>%
     dplyr::mutate(bin_min = structural_details_table$bin_min[bin_index],
            bin_max = structural_details_table$bin_max[bin_index],
            bin_mean = bin_min+(bin_max-bin_min)/2,
            nobs_pr = .$nobs*100/sum(.$nobs),
            nobs = 12*sqrt(.$nobs/3.14)/max(2*sqrt(.$nobs/3.14)))
-  
+
   structural_details_table %>%
     dplyr::slice(mean_shifts$bin_index) %>%
     dplyr::bind_cols(mean_shifts %>% dplyr::select(bin_mean, relative_shift, shift, nobs, nobs_pr))
