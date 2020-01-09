dc44a12c151a82743b76e7a35135809393319c41
Sebastian Ueckert <sebastian.ueckert@gmail.com>
2020-01-09 13:01:44 +0100

    add option to include id attribute when rendering HTML tables

--- R/qa_render.R
+++ R/qa_render.R
@@ -24,7 +24,7 @@ render_ofv_table <- function(qa_results, settings = qa_settings()){
       OFV = dplyr::if_else(!is.na(.data$ofv), format(.data$ofv), "ERROR")
     )
   
-  qa_kable(ofv_table, col.names = c("", "OFV"), booktabs=TRUE, longtable=TRUE, align=c("l","l"), linesep="") %>%
+  qa_kable(ofv_table, html_id = "ofv-table", col.names = c("", "OFV"), booktabs=TRUE, longtable=TRUE, align=c("l","l"), linesep="") %>%
     kableExtra::kable_styling(position="c",full_width = FALSE)
 }
 
@@ -95,8 +95,14 @@ add_blank_cols <- function(df, cols){
   tibble::add_column(df, !!!purrr::set_names(purrr::rep_along(cols, ""), cols))
 }
 
-qa_kable <- function(table,...) {
-  table_new <- knitr::kable(table,...)
+qa_kable <- function(table, html_id = NULL, ...) {
+  table_attr = ""
+  if(knitr::is_html_output()){
+    if(!is.null(html_id)) table_attr = glue::glue("id='{html_id}'")
+    table_new <- knitr::kable(table, table.attr = table_attr, ...)
+  }else{
+    table_new <- knitr::kable(table, ...)
+  }
   if(knitr::is_latex_output()) {
     if(!(ncol(table)>1 && all(is.null(colnames(table))))) {
       table_new <- kableExtra::row_spec(table_new, 0,bold=T)
