fc3dc64b36fbe54856d3caa398184ba1ad780cf4
Rikard Nordgren <rikard.nordgren@farmbio.uu.se>
2020-02-10 13:14:28 +0100

    Add functions on meta to get model name without runno and extension

--- R/common_meta.R
+++ R/common_meta.R
@@ -18,3 +18,21 @@ rplots_level <- function(meta) {
 model_path <- function(meta) {
     meta$model_files[1]
 }
+
+#' The prefix is what comes before the run number and the extension
+#' i.e. 'run' for 'run1.mod'
+#' @export
+model_prefix <- function(meta) {
+    no_ext <- tools::file_path_sans_ext(basename(model_path(meta)))
+    gsub("\\d+$", "", no_ext)
+}
+
+#' @export
+model_runno <- function(meta) {
+    no_ext <- tools::file_path_sans_ext(basename(model_path(meta)))
+    runno <- regmatches(no_ext, regexpr("\\d+", no_ext))
+    if (length(runno) == 0) {
+        runno <- ""
+    }
+    runno
+}
